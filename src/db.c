3b8b8ac68a2bd4ddace596f3fa8aee9533920e66
Fer <f.clerkx@student.fontys.nl>
2023-01-18 01:00:34 +0100

    Changed login for mysql

---
+++ src/db.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <mysql.h>
+#include <time.h>
+#include <string.h>
+
+int main() {
+	MYSQL *conn;
+	MYSQL_RES *res;
+	MYSQL_ROW row;
+	char *query;
+	char timestamp[20];
+	time_t now;
+
+	query = malloc(1000);
+
+	printf("%lu\n", (unsigned long)time(NULL));
+
+	conn = mysql_init(NULL);
+	if(conn == NULL) {
+		fprintf(stderr, "Can't initialize mySQL\n");
+		free(query);
+		return -1;
+	}
+
+	/* Connect to the database */
+	// Bad practice, hard coded credentials. Just for POC
+	if ( mysql_real_connect(conn, "127.0.0.1", "user", "password", "db", 0, NULL, 0) == NULL) {
+		fprintf(stderr, "%s\n", mysql_error(conn));
+		exit(1);
+	}
+	printf("Connected\n");
+
+	/* Get the current timestamp */
+	now = time(NULL);
+	sprintf(timestamp, "%ld", (long) now);
+
+	/* Form the query */
+	// Bad Practice, having hard coded values. Just for POC
+	query = "INSERT INTO ecg (ECGID, value, Timestamp) VALUES (1, 27, 1673958673)";
+
+	/* Execute the query */
+	if (mysql_query(conn, query)) {
+		fprintf(stderr, "%s\n", mysql_error(conn));
+		exit(1);
+	}
+
+	printf("Record inserted successfully.\n");
+
+	/* Close the connection */
+	mysql_close(conn);
+
+	free(query);
+
+	printf("Success\n");
+
+	return 0;
+}
